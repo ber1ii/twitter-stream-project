05ab9df4ba4d1c522659e037757e63368876a524
ber1ii <vujkovicgh@gmail.com>
2025-11-08 05:25:15 +0100

    Hotfix, removed depracated commands

--- server/config.js
+++ server/config.js
@@ -5,10 +5,7 @@ dotenv.config({ path: "../.env" });
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error("MongoDB connection error: ", error);
